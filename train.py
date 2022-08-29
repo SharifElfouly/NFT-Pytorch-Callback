93f44a91206c2030f087368b65a2146eaf3c56a0
shafu <selfouly@gmail.com>
2022-08-29 22:42:33 +0200

    setup

---
+++ train.py
@@ -0,0 +1,15 @@
+import os
+from torch import optim, nn, utils, Tensor
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+import pytorch_lightning as pl
+from model import Model
+from check import NftCallback
+
+# setup data
+dataset = MNIST(os.getcwd(), download=True, transform=ToTensor())
+train_loader = utils.data.DataLoader(dataset)
+
+# train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
+trainer = pl.Trainer(limit_train_batches=100, max_epochs=10, callbacks=[NftCallback()])
+trainer.fit(model=Model(), train_dataloaders=train_loader)
