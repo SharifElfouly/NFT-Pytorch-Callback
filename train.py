c91b48d3102825a111fa4cda8098aebc46e41dc5
shafu <selfouly@gmail.com>
2022-08-30 00:16:27 +0200

    prepare data

--- train.py
+++ train.py
@@ -1,18 +1,18 @@
-import os
 from torch import optim, nn, utils, Tensor
-from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 import pytorch_lightning as pl
 from model import Model
 from callback import NftCallback
+from torchvision import datasets, transforms
+from torch.utils.data import TensorDataset, DataLoader, random_split
+from data import prepare_data
 
-EPOCHS = 2
+EPOCHS = 20
 
-# setup data
-dataset = MNIST(os.getcwd(), download=True, transform=ToTensor())
-train_loader = utils.data.DataLoader(dataset)
-val_loader = utils.data.DataLoader(dataset)
+train, val, test = prepare_data()
+
+train_loader, val_loader, test_loader = DataLoader(train, batch_size=64), DataLoader(val, batch_size=64), DataLoader(test, batch_size=64)
 
 # train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
-trainer = pl.Trainer(limit_train_batches=10, max_epochs=EPOCHS, callbacks=[NftCallback()])
+trainer = pl.Trainer(max_epochs=EPOCHS, callbacks=[NftCallback()])
 trainer.fit(model=Model(), train_dataloaders=train_loader, val_dataloaders=val_loader)
