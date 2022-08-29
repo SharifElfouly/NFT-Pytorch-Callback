b3bad4b7df07365701c290ff3e50bcd0bc995cc1
shafu <selfouly@gmail.com>
2022-08-29 22:51:30 +0200

    callback works

--- callback.py
+++ callback.py
@@ -1,9 +1,7 @@
 from pytorch_lightning.callbacks import Callback
+from utils import hash_training
 
 
 class NftCallback(Callback):
-    def on_train_start(self, trainer, pl_module):
-        print("Training is starting")
-
-    def on_train_end(self, trainer, pl_module):
-        print("Training is ending")
+    def on_train_epoch_end(self, trainer, pl_module):
+        print(hash_training(trainer.model, "0x0000dfsdfsd", 44))
--- train.py
+++ train.py
@@ -6,10 +6,12 @@ import pytorch_lightning as pl
 from model import Model
 from callback import NftCallback
 
+EPOCHS = 2
+
 # setup data
 dataset = MNIST(os.getcwd(), download=True, transform=ToTensor())
 train_loader = utils.data.DataLoader(dataset)
 
 # train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
-trainer = pl.Trainer(limit_train_batches=100, max_epochs=10, callbacks=[NftCallback()])
+trainer = pl.Trainer(limit_train_batches=100, max_epochs=EPOCHS, callbacks=[NftCallback()])
 trainer.fit(model=Model(), train_dataloaders=train_loader)
